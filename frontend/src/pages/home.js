3f2829e1bbb66ac2054df68b7da6a09e32c4cd96
Emilie Smets <emiliesmets@MacBook-Pro-de-Emilie.local>
2019-11-30 16:22:32 +0100

    creat header

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,9 +1,8 @@
 import React from 'react'
+import Header from '../Component/header';
  
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <Header />
     )
 }
