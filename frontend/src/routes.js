544369500ea4bd17ab1abe6f5f6eed1a88aa718b
emmanuelm <emmanuel.muhirwa@gmail.com>
2019-12-05 20:41:53 +0100

    routes and header/footer centralization

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { Route, Switch } from "react-router-dom";
 import Home from "./pages/home";
 import Client from "./pages/client";
 import Restaurant from "./pages/restaurant";
@@ -11,16 +10,14 @@ import Ingredient from "./pages/ingredient";
 
 export default function MainRouter() {
     return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/client" component={Client} />
-                <Route exact path="/restaurant" component={Restaurant} />
-                <Route exact path="/recipe" component={Recipe} />
-                <Route exact path="/bill" component={Bill} />
-                <Route exact path="/ingredient" component={Ingredient} />
-                <Route exact path="/Order" component={Order} />
-            </Switch>
-        </Router>
+        <>
+            <Route exact path="/" component={Home} />
+            <Route path="/client" component={Client} />
+            <Route exact path="/restaurant" component={Restaurant} />
+            <Route exact path="/recipe" component={Recipe} />
+            <Route exact path="/bill" component={Bill} />
+            <Route exact path="/ingredient" component={Ingredient} />
+            <Route exact path="/order" component={Order} />
+        </>
     );
 }
